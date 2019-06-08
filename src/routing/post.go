97f7b3da6d8bf104ad271b1e45c0ff42a95331b9
Starz0r <starz0r@starz0r.com>
2019-06-08 00:33:29 -0500

    routing: insert entries from REST api

---
+++ src/routing/post.go
@@ -0,0 +1,36 @@
+package routing
+
+import (
+	"net/http"
+
+	"github.com/orchestrafm/music/src/database"
+	"github.com/spidernest-go/logger"
+	"github.com/spidernest-go/mux"
+)
+
+func createTrack(c echo.Context) error {
+	t := new(database.Track)
+	if err := c.Bind(t); err != nil {
+		logger.Error().
+			Err(err).
+			Msg("Invalid or malformed music track data.")
+
+		return c.JSON(http.StatusNotAcceptable, &struct {
+			Message string
+		}{
+			Message: "Music track data was invalid or malformed."})
+	}
+
+	err := t.Insert()
+	if err != nil {
+		return c.JSON(http.StatusNotAcceptable, &struct {
+			Message string
+		}{
+			Message: "Music track data did not get submitted to the database."})
+	}
+
+	return c.JSON(http.StatusOK, &struct {
+		Message string
+	}{
+		Message: "OK."})
+}
