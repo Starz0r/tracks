9fe27f72d937cacb730e8587fd6287a8fe9c0f6e
Starz0r <starz0r@starz0r.com>
2019-06-19 00:23:45 -0500

    database: update track information

--- src/database/metadata.go
+++ src/database/metadata.go
@@ -17,3 +17,18 @@ func (t *Track) New() error {
 
 	return err
 }
+
+func (t *Track) Update() error {
+	// TODO: Check if all struct fields are present
+	tracks := db.Collection("music")
+	rs := tracks.Find(t.ID)
+	// TODO: Check if DateCreated is not equal
+	t.DateModified = time.Now()
+	err := rs.Update(t)
+	if err != nil {
+		logger.Error().
+			Err(err).
+			Msg("Music Track could not be updated from the table.")
+	}
+	return err
+}
