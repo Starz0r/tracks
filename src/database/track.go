f207047ff51657b89b961fa3caf03576bf378270
Starz0r <starz0r@starz0r.com>
2019-06-19 00:24:47 -0500

    database: all fields on tracks can be omitted and empty

--- src/database/track.go
+++ src/database/track.go
@@ -5,18 +5,18 @@ import (
 )
 
 type Track struct {
-	ID             uint64    `db:"id" json:"id"`
-	DateCreated    time.Time `db:"date_created" json:"date_created"`
+	ID             uint64    `db:"id" json:"id,omitempty"`
+	DateCreated    time.Time `db:"date_created" json:"date_created,omitempty"`
 	DateModified   time.Time `db:"date_modified" json:"date_modified,omitempty"`
-	Jacket         string    `db:"jacket" json:"jacket"`
-	Genre          string    `db:"genre" json:"genre"`
+	Jacket         string    `db:"jacket" json:"jacket,omitempty"`
+	Genre          string    `db:"genre" json:"genre,omitempty"`
 	Credit         string    `db:"credit" json:"credit,omitempty"`
-	DisplayBPM     uint16    `db:"displaybpm" json:"displaybpm"`
-	Length         uint64    `db:"length" json:"length"`
-	Title          string    `db:"title" json:"title"`
+	DisplayBPM     uint16    `db:"displaybpm" json:"displaybpm,omitempty"`
+	Length         uint64    `db:"length" json:"length,omitempty"`
+	Title          string    `db:"title" json:"title,omitempty"`
 	TitleRomani    string    `db:"title_romani" json:"title_romani,omitempty"`
-	Artists        string    `db:"artists" json:"artists"`
-	ArtistsRomani  string    `db:"artists_romani" json:"artists_romani,omitempty"`
-	Subtitle       string    `db:"subtitle" json:"subtitle"`
-	SubtitleRomani string    `db:"subtitle_romani" json:"subtitle_romani,omitempty"`
+	Artists        string    `db:"artists" json:"artists,omitempty"`
+	ArtistsRomani  string    `db:"artists_romani,omitempty" json:"artists_romani,omitempty"`
+	Subtitle       string    `db:"subtitle" json:"subtitle,omitempty"`
+	SubtitleRomani string    `db:"subtitle_romani,omitempty" json:"subtitle_romani,omitempty"`
 }
