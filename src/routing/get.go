9179f04374382ea1d1f219ff47b94e6e9ea2b82a
Starz0r <starz0r@starz0r.com>
2019-06-21 23:25:16 -0500

    routing: get a track from an id

---
+++ src/routing/get.go
@@ -0,0 +1,38 @@
+package routing
+
+import (
+	"database/sql"
+	"net/http"
+	"strconv"
+
+	"github.com/orchestrafm/music/src/database"
+	"github.com/spidernest-go/logger"
+	"github.com/spidernest-go/mux"
+)
+
+func getTrackByID(c echo.Context) error {
+	id, err := strconv.ParseUint(c.Param("id"), 10, 64)
+	if err != nil {
+		logger.Error().
+			Err(err).
+			Msg("Invalid Parameters for getting a track.")
+
+		return c.JSON(http.StatusNotAcceptable, &struct {
+			Message string
+		}{
+			Message: "Invalid or malformed music track data."})
+	}
+
+	t, err := database.SelectID(id)
+	if err != nil && err != sql.ErrNoRows {
+		logger.Error().
+			Err(err).
+			Msg("")
+		return c.JSON(http.StatusNotAcceptable, &struct {
+			Message string
+		}{
+			Message: "Invalid or malformed music track data."})
+	}
+
+	return c.JSON(http.StatusOK, t)
+}
