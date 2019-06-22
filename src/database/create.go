0ac7602fff762fd92f21dee6c4c865e20943eaf9
Starz0r <starz0r@starz0r.com>
2019-06-21 21:37:37 -0500

    database: split metadata into create and edit

---
+++ src/database/create.go
@@ -0,0 +1,23 @@
+package database
+
+import (
+	"time"
+
+	"github.com/spidernest-go/logger"
+)
+
+func (t *Track) New() error {
+	t.DateCreated = time.Now()
+	t.DateModified = time.Unix(0, 0)
+	_, err := db.InsertInto("music").
+		Values(t).
+		Exec()
+
+	if err != nil {
+		logger.Error().
+			Err(err).
+			Msg("Music Track could not be inserted into the table.")
+	}
+
+	return err
+}
--- src/database/edit.go
+++ src/database/edit.go
@@ -6,22 +6,6 @@ import (
 	"github.com/spidernest-go/logger"
 )
 
-func (t *Track) New() error {
-	t.DateCreated = time.Now()
-	t.DateModified = time.Unix(0, 0)
-	_, err := db.InsertInto("music").
-		Values(t).
-		Exec()
-
-	if err != nil {
-		logger.Error().
-			Err(err).
-			Msg("Music Track could not be inserted into the table.")
-	}
-
-	return err
-}
-
 func (t *Track) Update() error {
 	// TODO: Check if all struct fields are present
 	tracks := db.Collection("music")
@@ -36,6 +20,7 @@ func (t *Track) Update() error {
 	}
 	return err
 }
+
 func (t *Track) Edit() error {
 	tracks := db.Collection("music")
 	rs := tracks.Find(t.ID)
