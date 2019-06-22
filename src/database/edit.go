af07900b660ef9694f86d53346cb7ddba9f6a34a
Starz0r <starz0r@starz0r.com>
2019-06-21 23:06:05 -0500

    database: comments

--- src/database/edit.go
+++ src/database/edit.go
@@ -8,6 +8,7 @@ import (
 
 func (t *Track) Update() error {
 	// TODO: Check if all struct fields are present
+	// TODO: Abort if the Track ID isn't already present in the database
 	tracks := db.Collection("music")
 	rs := tracks.Find(t.ID)
 	// TODO: Check if DateCreated is not equal
@@ -22,6 +23,7 @@ func (t *Track) Update() error {
 }
 
 func (t *Track) Edit() error {
+	// Take a uint64 for ID instead of t.ID
 	tracks := db.Collection("music")
 	rs := tracks.Find(t.ID)
 	// TODO: Check if DateCreated is not equal
