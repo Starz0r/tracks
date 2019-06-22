6d9b8ef69860334662d049db528d756046535699
Starz0r <starz0r@starz0r.com>
2019-06-21 21:32:21 -0500

    database: update track information without some constraints

--- src/database/metadata.go
+++ src/database/metadata.go
@@ -36,3 +36,16 @@ func (t *Track) Update() error {
 	}
 	return err
 }
+func (t *Track) Edit() error {
+	tracks := db.Collection("music")
+	rs := tracks.Find(t.ID)
+	// TODO: Check if DateCreated is not equal
+	t.DateModified = time.Now()
+	err := rs.Update(t)
+	if err != nil {
+		logger.Error().
+			Err(err).
+			Msg("Music Track could not be updated from the table.")
+	}
+	return err
+}
