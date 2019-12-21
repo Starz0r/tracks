ecf47de18c90edbad833afb0fdf61b6903249040
Starz0r <starz0r@starz0r.com>
2019-12-21 02:20:32 -0600

    migrations: change publisher data type and reference to uuid

--- src/database/migrations/1576913948_002_owner.sql
+++ src/database/migrations/1576913948_002_owner.sql
@@ -1,4 +1,4 @@
 ALTER TABLE `tracks` ADD (
-    `publisher` INT(8) UNSIGNED NOT NULL,
-    FOREIGN KEY (`publisher`) REFERENCES `profiles` (`id`)
+    `publisher` VARCHAR(255) UNSIGNED NOT NULL,
+    FOREIGN KEY (`publisher`) REFERENCES `profiles` (`uuid`)
 )
