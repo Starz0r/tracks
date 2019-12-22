b7fdf80a822abf4d06b8c56de3071dbb3f762f5a
Starz0r <starz0r@starz0r.com>
2019-12-22 01:36:59 -0600

    routing: solve interfaces not being casted into structures correctly
    I honestly don't get how this could be working on the same Go version I've been testing on, and then all of a sudden, out of the blue, it just stops working. It makes no sense at all. Casting a map[string]interface{} type annotated with JSON hints should work automatically and just fine right? I'm not crazy, this did work, didn't it? Whatever the case now I get to enjoy rewriting a bunch of sections that used to work, and solve problems that didn't exist until Go just invented them while I was sitting doing my work.
    
    Before, authenticated API requests would only 0ms on my local machine, which is very fast to say the least. Now with this new solution, a 16ms performance penalty is incured, and I had to introduce yet another dependency that has to be compiled, resulting in a larger binary size. That may not seem like a lot, but in a production environment where latency over the network, memory pressure, other processes taking CPU time, and database queries, this number could easily be inflated, and now I've inflated it even more. One day in the future I'll be able to get to the bottom of this issue, maybe Google will have even solved this problem. Maybe I'm just too angry, but this issue did take up around 8 hours of my time, just for me to have to default on a inferior solution simply because "magic", fun.

--- src/routing/auth.go
+++ src/routing/auth.go
@@ -5,21 +5,59 @@ import (
 	"strings"
 
 	"github.com/dgrijalva/jwt-go"
+	"github.com/mitchellh/mapstructure"
+	"github.com/spidernest-go/logger"
 	"github.com/spidernest-go/mux"
 )
 
 type jwtExtendedClaims struct {
 	RealmAccess struct {
-		Roles []string `json:"roles"`
-	} `json:"realm_access"`
+		Roles []string `json:"roles,omitempty"`
+	} `json:"realm_access,omitempty"`
 	Scope             string `json:"scope"`
-	PreferredUsername string `json:"preferred_username"`
-	jwt.StandardClaims
+	PreferredUsername string `json:"preferred_username,omitempty"`
+	Audience          string `json:"aud,omitempty"`
+	ExpiresAt         int64  `json:"exp,omitempty"`
+	Id                string `json:"jti,omitempty"`
+	IssuedAt          int64  `json:"iat,omitempty"`
+	Issuer            string `json:"iss,omitempty"`
+	NotBefore         int64  `json:"nbf,omitempty"`
+	Subject           string `json:"sub,omitempty"`
+	//jwt.StandardClaims
+}
+
+func decodeToClaims(src, dst interface{}) error {
+	dec, err := mapstructure.NewDecoder(&mapstructure.DecoderConfig{
+		TagName: "json",
+		Result:  dst,
+	})
+	if err != nil {
+		logger.Error().
+			Err(err).
+			Msg("mapstructure decoder could not be initialized")
+		return err
+	}
+	err = dec.Decode(src)
+	if err != nil {
+		logger.Error().
+			Err(err).
+			Msg("structure could not be decoded")
+		return err
+	}
+	return nil
 }
 
 func FullAuthCheck(c echo.Context) error {
 	user := c.Get("user").(*jwt.Token)
-	claims := user.Claims.(*jwtExtendedClaims)
+	//claims := user.Claims.(*jwtExtendedClaims)
+	claims := new(jwtExtendedClaims)
+	err := decodeToClaims(user.Claims, claims)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, &struct {
+			Message string
+		}{
+			Message: "Authorization Token could not be parsed."})
+	}
 
 	auth := strings.Contains(claims.Scope, "track:write") || strings.Contains(claims.Scope, "track:admin")
 	admin := strings.Contains(claims.Scope, "track:admin")
@@ -34,7 +72,12 @@ func FullAuthCheck(c echo.Context) error {
 
 func AuthorizationCheck(c echo.Context) (bool, bool) {
 	user := c.Get("user").(*jwt.Token)
-	claims := user.Claims.(*jwtExtendedClaims)
+	//claims := user.Claims.(*jwtExtendedClaims)
+	claims := new(jwtExtendedClaims)
+	err := decodeToClaims(user.Claims, claims)
+	if err != nil {
+		return false, false
+	}
 
 	auth := strings.Contains(claims.Scope, "track:write") || strings.Contains(claims.Scope, "track:admin")
 	admin := strings.Contains(claims.Scope, "track:admin")
@@ -44,5 +87,10 @@ func AuthorizationCheck(c echo.Context) (bool, bool) {
 
 func SelfAuthCheck(c echo.Context) *jwtExtendedClaims {
 	user := c.Get("user").(*jwt.Token)
-	return user.Claims.(*jwtExtendedClaims)
+	claims := new(jwtExtendedClaims)
+	err := decodeToClaims(user.Claims, claims)
+	if err != nil {
+		return nil
+	}
+	return claims
 }
