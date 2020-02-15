e0e73ac125791e3702b210bfa2a95e0fe8e01a40
Starz0r <starz0r@starz0r.com>
2020-02-15 14:29:58 -0600

    database: ability to find tracks by recently submitted

--- src/database/find.go
+++ src/database/find.go
@@ -42,3 +42,22 @@ func SelectName(name string) ([]*Track, error) {
 		return ts, nil
 	}
 }
+
+func SelectRecent(limit int) ([]*Track, error) {
+	var ts []*Track
+	tracks := db.Collection("tracks")
+	rs := tracks.Find().OrderBy("id", "DESC").Limit(limit)
+
+	err := rs.All(&ts)
+	if err != nil && err != sql.ErrNoRows {
+		logger.Error().
+			Err(err).
+			Msg("Bad parameters or database error.")
+	}
+
+	if err == sql.ErrNoRows {
+		return nil, err
+	} else {
+		return ts, nil
+	}
+}
